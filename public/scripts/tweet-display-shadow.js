31b65dbf22a76da083d61995392d4f1621c8e9fc
Jill Regan <jillian.jnr@hotmail.com>
2023-12-05 22:30:41 -0500

    Create a box shadow over tweet display box when user hovers with mouse

---
+++ public/scripts/tweet-display-shadow.js
@@ -0,0 +1,15 @@
+
+
+
+$(document).ready(function() {
+  
+  $('.view-tweet').on('mouseenter', function(){
+   $('.view-tweet').css('box-shadow', '5px 5px')
+  })
+
+  $('.view-tweet').on('mouseleave', function(){
+    $('.view-tweet').css('box-shadow', '0px 0px')
+   })
+
+
+});
