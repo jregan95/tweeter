9c4d93fa6c0c5919903b7115e30a03ad701806b1
Jill Regan <jillian.jnr@hotmail.com>
2023-12-07 01:07:42 -0500

    Update comments to provde more clear information of what CSS styles are doing

--- public/scripts/tweet-display-shadow.js
+++ public/scripts/tweet-display-shadow.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
   /*When users mouse hovers over posted specific tweets in the main section
   of the page the box shadow appears around the tweet */
   $(document).on('mouseenter', '.view-tweet', function(){
-   $(this).css('box-shadow', '10px 10px lightgrey')
-  })
+   $(this).css('box-shadow', '10px 10px lightgrey');
+  });
 
   //Box shadow disspears upon user mouse leaving tweet box
   $(document).on('mouseleave', '.view-tweet', function(){
-    $(this).css('box-shadow', '0px 0px')
-   })
+    $(this).css('box-shadow', '0px 0px');
+   });
 
 
 });
