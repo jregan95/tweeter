245c31565fe0c4043b55e10f28f9adc0e946e112
Jill Regan <jillian.jnr@hotmail.com>
2023-12-07 01:00:04 -0500

    Update the comments in error-message.js to it is clear that the JQuery is clearing the error message

--- public/scripts/error-messages.js
+++ public/scripts/error-messages.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   
-  /*This clears the error message for character count when user
-  begins typing again*/ 
+  /*This clears the error message (which would have been displayed if
+    user was over or under on character count) for character count when 
+    user begins typing again*/ 
   $('#new-tweet-submission-form').keypress(function() {
     $('.err-to-much-text').text(''); 
     $('.err-no-text').text('');
   });
-
 
 });
